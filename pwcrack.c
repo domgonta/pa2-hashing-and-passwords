e81f013eee554660f06eca1f8bc006fc3fbc696b
domgonta <156019673+domgonta@users.noreply.github.com>
2024-11-13 18:31:00 +0000

    new

--- pwcrack.c
+++ pwcrack.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -9,7 +8,7 @@
 uint8_t hex_to_byte(unsigned char h1, unsigned char h2);
 void hexstr_to_hash(char hexstr[], unsigned char hash[32]);
 int8_t check_password(char password[], unsigned char given_hash[32]);
-int8_t crack_password(char password[], unsigned char given_hash[]);
+int8_t crack_password(char password[], unsigned char given_hash[32]);
 
 void test_hex_to_byte() {
     assert(hex_to_byte('c', '8') == 200);
@@ -42,7 +41,6 @@ void test_check_password() {
 const int SHA_LENGTH = 32;
 const int testing = 1;
 
-
 uint8_t hex_to_byte(unsigned char h1, unsigned char h2) {
     uint8_t v1, v2;
     if (h1 >= 'a' && h1 <= 'f') v1 = h1 - 87;
@@ -62,15 +60,16 @@ void hexstr_to_hash(char hexstr[], unsigned char hash[32]) {
 
 int8_t check_password(char password[], unsigned char given_hash[32]) {
     unsigned char hash[SHA_LENGTH];
-    SHA256(password, strlen(password), hash);
+    SHA256((unsigned char*)password, strlen(password), hash);
     int i;
-    for (i = 0; i < 32; i++) {
+    for (i = 0; i < SHA_LENGTH; i++) {
         if (given_hash[i] != hash[i]) {
             return 0;
         }
     }
     return 1;
 }
+
 int8_t crack_password(char password[], unsigned char given_hash[32]) {
     if (check_password(password, given_hash)) {
         return 1;
@@ -114,12 +113,15 @@ int8_t crack_password(char password[], unsigned char given_hash[32]) {
 }
 
 int main(int argc, char** argv) {
-
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <hash>\n", argv[0]);
+        return 1;
+    }
 
     unsigned char given_hash[32];
     hexstr_to_hash(argv[1], given_hash);
 
-    char password[128]; 
+    char password[128];
     while (fgets(password, sizeof(password), stdin)) {
         password[strcspn(password, "\n")] = 0;
 
